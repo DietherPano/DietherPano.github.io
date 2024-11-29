a551012a0233925e9c08346bb76f916601f3afb6
DietherPano <dietherpano95@gmail.com>
2024-11-29 21:49:11 +0800

    rework Portfolio (no mobile view for now)

---
+++ js/scroll.js
@@ -0,0 +1,15 @@
+document.getElementById('Home').addEventListener('click', () => {
+    document.getElementById('header-section').scrollIntoView({behavior: 'smooth'})
+})
+document.getElementById('About').addEventListener('click', () => {
+    document.getElementById('about-me-section').scrollIntoView({behavior: 'smooth'})
+})
+document.getElementById('Projects').addEventListener('click', () => {
+    document.getElementById('projects-section').scrollIntoView({behavior: 'smooth'})
+})
+document.getElementById('Contacts').addEventListener('click', () => {
+    document.getElementById('contact-section').scrollIntoView({behavior: 'smooth'})
+})
+document.getElementById('goup').addEventListener('click', () => {
+    document.getElementById('header-section').scrollIntoView({behavior: 'smooth'})
+})
